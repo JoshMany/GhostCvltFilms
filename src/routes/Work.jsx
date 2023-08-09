4ac6580976bf73d5107554b130144290bf2d5bc5
Jose Manuel <jose.munoz@techgenies.com>
2023-08-09 01:14:04 -0600

    Subida pagina Work

--- src/routes/Work.jsx
+++ src/routes/Work.jsx
@@ -2,6 +2,7 @@ import Topbar from "../components/Topbar";
 import Footer from "../components/Footer";
 import Marquee from "react-fast-marquee";
 import { ReactFitty } from "react-fitty";
+import YouTube from "react-youtube";
 
 function Work() {
   const Divider = ({ text }) => {
@@ -23,30 +24,39 @@ function Work() {
     );
   };
 
-  const GridElement = ({ title, type }) => {
-    return (
-      <div className="aspect-video border text-white text-xl">
-        <p>{type}</p>
-        <p>{title}</p>
-      </div>
-    );
-  };
+  const YoutubeEmbed = ({ embedId }) => (
+    <div className="aspect-video">
+      <iframe
+        src={`https://www.youtube.com/embed/${embedId}`}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+        className="w-full h-full"
+      />
+    </div>
+  );
+
+  const videosIds = [
+    "0zGAEU2XMYs",
+    "USqtuBbb3H8",
+    "xBmAPZb1X4g",
+    "ymu8Tq8lvVA",
+  ];
 
   return (
     <>
       <div className="relative">
         <Topbar />
         <div className="pt-14 px-4 flex flex-col w-full h-full">
-          <div className="text-white uppercase flex-1 font-CooperHewittBook block lg:hidden">
+          <div className="text-white uppercase w-full font-CooperHewittBook block lg:hidden">
             <ReactFitty>Our Work</ReactFitty>
           </div>
-          <div className="text-white uppercase w-full h-min font-CooperHewittBook hidden lg:block">
-            <h1 className="text-[140px]">Our Work</h1>
+          <div className="text-white uppercase w-full h-auto font-CooperHewittBook hidden lg:block">
+            <h1 className="text-[140px] my-4">Our Work</h1>
           </div>
-          <div className="grid grid-cols-1 gap-8 mt-4 mb-8 md:grid-cols-2 lg:grid-cols-3">
-            <GridElement title={"Uno"} type={"Uno"} />
-            <GridElement title={"Uno"} type={"Uno"} />
-            <GridElement title={"Uno"} type={"Uno"} />
+          <div className="grid grid-cols-1 gap-8 mt-4 mb-8 md:grid-cols-2">
+            <YoutubeEmbed embedId={videosIds[0]} />
+            <YoutubeEmbed embedId={videosIds[1]} />
             <div className="aspect-video h-min w-full">
               <ReactFitty
                 wrapText={true}
@@ -58,10 +68,8 @@ function Work() {
                 Bring your ideas to <b className="text-fuchsia-500">life</b>!
               </ReactFitty>
             </div>
-            <GridElement title={"Uno"} type={"Uno"} />
-            <GridElement title={"Uno"} type={"Uno"} />
-            <GridElement title={"Uno"} type={"Uno"} />
-            <GridElement title={"Uno"} type={"Uno"} />
+            <YoutubeEmbed embedId={videosIds[2]} />
+            <YoutubeEmbed embedId={videosIds[3]} />
           </div>
         </div>
         <Divider text={"JOIN THE CVLT"} />
