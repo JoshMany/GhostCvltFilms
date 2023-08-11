7df4bfd0c7369d3ed89eb76a0b6393ba5f76e79b
Jose Manuel <jose.munoz@techgenies.com>
2023-08-10 18:33:28 -0600

    Little Fix for Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,18 +5,18 @@ export default function Hero() {
     <>
       <header className="relative flex h-screen overflow-hidden z-0 text-white">
         <div className="relative mx-8 z-20 flex flex-col sm:my-24 sm:flex-1 sm:w-screen sm:items-center md:my-32  lg:mx-[100px]">
-          <div className="w-full md:flex-1">
+          <div className="relative flex-1">
             <p className="font-CooperHewittBook uppercase text-[65px] leading-[65px] mt-14 sm:mt-0 sm:align-bottom sm:w-full md:w-[500px] md:leading-[80px] md:text-left md:whitespace-pre-line">
               Amazing content for amazing brands.
             </p>
           </div>
-          <div className="w-full md:flex-1 flex justify-end items-end">
-            <p className="font-sm text-[30px] leading-[30px] uppercase mt-12 text-right sm:mt-24 sm:w-[300px] md:mt-24">
+          <div className="relative flex-1 flex justify-end">
+            <p className="text-[20px] leading-[30px] uppercase mt-4 text-right sm:mt-24 sm:w-[300px] md:mt-24">
               We are a production company located in mexico city
             </p>
           </div>
 
-          <div className="absolute flex self-center justify-center items-center bottom-10 mb-5 md:left-8 md:bottom-0">
+          <div className="relative flex-1 flex self-center justify-center items-center">
             <ReactCurvedText
               width={200}
               height={200}
@@ -36,7 +36,7 @@ export default function Hero() {
             <img
               src="/arrow.svg"
               alt="ArrowIcon"
-              className="bg-white rounded-full w-[110px]"
+              className="bg-transparent rounded-full"
             />
           </div>
         </div>
